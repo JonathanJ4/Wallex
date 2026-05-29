89ffeeb9003c75cc446b8dfa82302299ad4b532c
JonathanJ4 <Jonathanthomasv6@gmail.com>
2026-05-29 18:17:23 -0400

    Initialize project structure with Maven, including wrapper scripts and configuration files

---
+++ src/main/java/com/wallex/WallexApplication.java
@@ -0,0 +1,13 @@
+package com.wallex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallex/WallexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
