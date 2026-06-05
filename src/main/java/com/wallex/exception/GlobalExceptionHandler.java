27431e73d9ecab56038cf8e88776eecce8271a41
JonathanJ4 <Jonathanthomasv6@gmail.com>
2026-06-04 22:48:15 -0400

    Change RestController to RestControllerAdvice in GlobalExceptionHandler for improved exception handling

--- src/main/java/com/wallex/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wallex/exception/GlobalExceptionHandler.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler{
 
     @ExceptionHandler(TransactionNotFoundException.class)
