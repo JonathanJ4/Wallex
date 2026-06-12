121ad87b42e7b6bc549279e505d24e66030b1beb
JonathanJ4 <Jonathanthomasv6@gmail.com>
2026-06-11 23:42:38 -0400

    Add repository methods for transaction filtering by category, type, merchant, and date range

--- src/main/java/com/wallex/repository/TransactionRepository.java
+++ src/main/java/com/wallex/repository/TransactionRepository.java
@@ -2,8 +2,15 @@ package com.wallex.repository;
 
 import com.wallex.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.wallex.enums.TransactionType;
+import java.time.LocalDate;
+import java.util.*;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+
+List<Transaction> findByCategoryIgnoreCase(String category);
+ List<Transaction> findByType(TransactionType type);
+List<Transaction> findByMerchantContainingIgnoreCase(String Merchant);
+List<Transaction> findByTransactionDateBetween(LocalDate startDate, LocalDate endDate);
 
 }
