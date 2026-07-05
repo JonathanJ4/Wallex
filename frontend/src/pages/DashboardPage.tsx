ea325170db00cc300996fbf2564ae754326f0dae
Jonathan <jonathanthomasv6@gmail.com>
2026-07-05 19:37:12 -0400

    feat: implement DashboardPage layout with sidebar and content sections

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,5 +1,29 @@
-function DashboardPage() {
-  return <h1>Dashboard</h1>;
-}
+import "./DashboardPage.css";
 
-export default DashboardPage;
+function DashboardPage(){
+
+  return (
+    <main className="dashboard-page">
+      <aside className="sidebar">
+        <h1 className="sidebar-logo">Wallex</h1>
+
+        <nav className="sidebar-nav">
+          <button className="nav-item active ">Dashboard</button>
+          <button className="nav-item">Transactions</button>
+          <button className="nav-item">Subscriptions</button>
+        </nav>
+      </aside>
+
+      <section className="dashboard-content">
+        <div className="dashboard-header">
+          <div>
+            <h2>Dashboard</h2>
+            <p>Welcome back, Jonathan</p>
+          </div>
+
+          <button className="month-button">July 2026</button>
+        </div>
+        </section>
+    </main>
+  )
+}
