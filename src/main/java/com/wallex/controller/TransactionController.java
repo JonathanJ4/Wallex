7672626321455c7d937f0550ee854b98b6ec07db
Jonathan <jonathanthomasv6@gmail.com>
2026-07-08 20:05:33 -0400

    CORS X_X

--- src/main/java/com/wallex/controller/TransactionController.java
+++ src/main/java/com/wallex/controller/TransactionController.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/transactions")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TransactionController {
 
     private final TransactionService transactionService;
